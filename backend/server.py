9086d4a5acfcbeb6f058589622f5a0012c7ee75f
c-m-w <c-m-w-other@tuta.io>
2023-02-19 23:43:15 -0700

    Create server.py

---
+++ backend/server.py
@@ -0,0 +1,22 @@
+# server.py
+
+import subprocess
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def client(path):
+
+    proc = subprocess.Popen(["./test"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    out, error = proc.communicate()
+
+    return "output: " + out.decode("ascii")
+
+if __name__ == "__main__":
+
+    server_port = int(os.environ.get("PORT", 5000)) 
+    app.run(debug=False, host="0.0.0.0", port=server_port)
